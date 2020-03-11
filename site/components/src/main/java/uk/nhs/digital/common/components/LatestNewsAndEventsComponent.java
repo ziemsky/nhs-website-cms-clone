68b19d9b9c1fe293b12538be6bf007c4dda86593
Joseph Cyrus <Joseph.Cyrus@valtech.com>
2020-03-11 15:26:35 +0000

    [DW-1236] Fix case sensitive doctype

--- site/components/src/main/java/uk/nhs/digital/common/components/LatestNewsAndEventsComponent.java
+++ site/components/src/main/java/uk/nhs/digital/common/components/LatestNewsAndEventsComponent.java
@@ -1,16 +1,12 @@
 package uk.nhs.digital.common.components;
 
 import org.hippoecm.hst.component.support.bean.BaseHstComponent;
-import org.hippoecm.hst.content.beans.standard.HippoBean;
 import org.hippoecm.hst.core.component.HstRequest;
 import org.hippoecm.hst.core.component.HstResponse;
 import org.hippoecm.hst.core.request.HstRequestContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import uk.nhs.digital.website.beans.Blog;
 import uk.nhs.digital.website.beans.HubNewsAndEvents;
-
-import java.util.List;
 
 public class LatestNewsAndEventsComponent extends BaseHstComponent {
 
