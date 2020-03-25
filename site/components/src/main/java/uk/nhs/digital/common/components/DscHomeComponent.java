a4fef7da8acd1ea802221939af058ac2915b904c
simon.lacey <simon.lacey@valtech.com>
2020-03-25 13:58:27 +0000

    [DW 1286] Build of a java component to search for cyber alerts. Access is through a request document parameter containing the DSCHome bean

--- site/components/src/main/java/uk/nhs/digital/common/components/DscHomeComponent.java
+++ site/components/src/main/java/uk/nhs/digital/common/components/DscHomeComponent.java
@@ -1,7 +1,5 @@
 package uk.nhs.digital.common.components;
 
-import static org.apache.commons.collections.IteratorUtils.toList;
-
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang3.math.NumberUtils;
 import org.hippoecm.hst.component.support.bean.BaseHstComponent;
@@ -10,6 +8,7 @@ import org.hippoecm.hst.content.beans.query.HstQueryResult;
 import org.hippoecm.hst.content.beans.query.builder.HstQueryBuilder;
 import org.hippoecm.hst.content.beans.query.exceptions.QueryException;
 import org.hippoecm.hst.content.beans.standard.HippoBean;
+import org.hippoecm.hst.content.beans.standard.HippoBeanIterator;
 import org.hippoecm.hst.core.component.HstRequest;
 import org.hippoecm.hst.core.component.HstResponse;
 import org.hippoecm.hst.core.request.HstRequestContext;
@@ -18,11 +17,14 @@ import org.slf4j.LoggerFactory;
 import uk.nhs.digital.website.beans.CyberAlert;
 import uk.nhs.digital.website.beans.DscHome;
 
-import java.util.Calendar;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 
 public class DscHomeComponent extends BaseHstComponent {
 
-    private static Logger log = LoggerFactory.getLogger(DscHomeComponent.class);
+    private static final Logger log = LoggerFactory.getLogger(TaxonomyFacetWrapper.class);
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     @Override
     public void doBeforeRender(HstRequest request, HstResponse response) {
@@ -30,32 +32,49 @@ public class DscHomeComponent extends BaseHstComponent {
 
         final HstRequestContext ctx = request.getRequestContext();
         DscHome dscHome = ctx.getContentBean(DscHome.class);
+        int pageSize = getListSize();
+        ArrayList list = new ArrayList(pageSize);
+        LocalDateTime now = LocalDateTime.now();
+
         if (dscHome != null) {
+
+            try {
+
+                HippoBean scope = ctx.getSiteContentBaseBean();
+                HstQuery hstCyberAlertQuery = HstQueryBuilder.create(scope)
+                    .ofTypes(CyberAlert.class)
+                    .limit(pageSize).orderByDescending(
+                        now.format(formatter))
+                    .build();
+
+                HstQueryResult cyberAlertResult = hstCyberAlertQuery.execute();
+                HippoBeanIterator iterator = cyberAlertResult.getHippoBeans();
+
+                while (iterator.hasNext()) {
+                    list.add(iterator.next());
+                }
+
+                dscHome.setCyberAlerts(list);
+
+            } catch (QueryException qe) {
+                log.info(qe.getMessage());
+                qe.printStackTrace();
+
+            }
+
             request.setAttribute("document", dscHome);
-        }
-
-        try {
-            HippoBean scope = ctx.getSiteContentBaseBean();
-
-            HstQuery hstAlertsQuery = HstQueryBuilder.create(scope)
-                .ofTypes(CyberAlert.class).orderByDescending(Calendar.getInstance().toString())
-                .limit(getAlertPageSize(request))
-                .build();
-
-            HstQueryResult alertsResult = hstAlertsQuery.execute();
-            request.setAttribute("cyberAlertsList", toList(alertsResult.getHippoBeans()));
-
-        } catch (QueryException queryException) {
-            log.warn("QueryException ", queryException);
         }
     }
 
-    protected int getAlertPageSize(final HstRequest request) {
+
+    protected int getListSize() {
         //getting the componentPageSize parameter value if defined in the component
-        String compononentPageSize = StringUtils.defaultIfEmpty(getComponentLocalParameter("alertPageSize"), "");
+        String compononentPageSize = StringUtils
+            .defaultIfEmpty(getComponentParameter("alertPageSize"), "");
         //if the componentPageSize hasn't been defined, then use the component param info
         return NumberUtils.isCreatable(compononentPageSize)
             ? Integer.parseInt(compononentPageSize) : 3;
     }
 
 }
+
--- site/components/src/main/java/uk/nhs/digital/website/beans/DscHome.java
+++ site/components/src/main/java/uk/nhs/digital/website/beans/DscHome.java
@@ -1,74 +1,29 @@
 package uk.nhs.digital.website.beans;
 
-import static org.apache.commons.collections.IteratorUtils.toList;
-
-import org.hippoecm.hst.container.RequestContextProvider;
 import org.hippoecm.hst.content.beans.Node;
-import org.hippoecm.hst.content.beans.query.HstQuery;
-import org.hippoecm.hst.content.beans.query.HstQueryResult;
-import org.hippoecm.hst.content.beans.query.builder.HstQueryBuilder;
-import org.hippoecm.hst.content.beans.query.exceptions.QueryException;
 import org.hippoecm.hst.content.beans.standard.HippoBean;
 import org.hippoecm.hst.content.beans.standard.HippoGalleryImageSet;
-import org.hippoecm.hst.core.component.HstComponentException;
-import org.hippoecm.hst.core.request.HstRequestContext;
 import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
 
-import java.util.Calendar;
 import java.util.List;
 
 @HippoEssentialsGenerated(internalName = "website:dschomepage")
 @Node(jcrType = "website:dschomepage")
 public class DscHome extends CommonFieldsBean {
 
+    private List<HippoBean> cyberAlerts;
+
     @HippoEssentialsGenerated(internalName = "website:bannerimagelink")
     public HippoGalleryImageSet getTrustimage() {
         return getLinkedBean("website:bannerimagelink", HippoGalleryImageSet.class);
     }
 
-    @HippoEssentialsGenerated(internalName = "website:usedservicesinternallink")
-    public List<HippoBean> getUsedServiceInternalLinks() {
-        return getLinkedBeans("website:usedservicesinternallink", HippoBean.class);
+    public void setCyberAlerts(List<HippoBean> cyberAlerts) {
+        this.cyberAlerts = cyberAlerts;
     }
 
-    @HippoEssentialsGenerated(internalName = "website:usedservicesexternallink")
-    public List<ResourceExternalLink> getUsedServicesExternalLinks() {
-        return getLinkedBeans("website:usedservicesexternallink", ResourceExternalLink.class);
+    public List<HippoBean> getCyberAlerts() {
+        return cyberAlerts;
     }
-
-    @HippoEssentialsGenerated(internalName = "website:usefulinformationinternallink")
-    public List<HippoBean> getUsefulInformationInternalLinks() {
-        return getLinkedBeans("website:usefulinformationinternallink", HippoBean.class);
-    }
-
-    @HippoEssentialsGenerated(internalName = "website:usefulinformationexternallink")
-    public List<ResourceExternalLink> getUsefulInformationExternalLinks() {
-        return getLinkedBeans("website:usefulinformationexternallink", ResourceExternalLink.class);
-    }
-
-    @HippoEssentialsGenerated(internalName = "website:homepagecategories")
-    public List<DscCategory> getHomepageCategories() {
-        return getLinkedBeans("website:homepagecategories", DscCategory.class);
-    }
-
-    public List<HippoBean> getCaseStudies() throws HstComponentException, QueryException {
-        return getCaseStudyList();
-    }
-
-    public List<HippoBean> getCaseStudyList() throws QueryException {
-
-        HstRequestContext requestContext = RequestContextProvider.get();
-        HippoBean scope = requestContext.getSiteContentBaseBean();
-
-        HstQuery hstBlogQuery = HstQueryBuilder.create(scope)
-            .ofTypes(CaseStudy.class).orderByDescending(Calendar.getInstance().toString())
-            .limit(2)
-            .build();
-
-        HstQueryResult blogResult = hstBlogQuery.execute();
-        return toList(blogResult.getHippoBeans());
-
-    }
-
 
 }
